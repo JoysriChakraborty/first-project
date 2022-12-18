ef521204e1633f36d6a5b7e2013b1c0a954dd0d2
JoysriChakraborty <120707882+JoysriChakraborty@users.noreply.github.com>
2022-12-18 05:16:19 +0000

    Add files via upload

---
+++ service_add.php
@@ -0,0 +1,41 @@
+<?php
+require_once('../includes/header.php');
+
+?>
+<div class="app-content">
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="page-description">
+                        <h1>Services</h1>
+                    </div>
+                </div>
+            </div>
+<div class="row justify-content-center">
+
+    <div class="col-10">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">Add Service</h5>
+            </div>
+            <div class="card-body">
+                <div class="example-container">
+                    <div class="example-content">
+                        <input type="email" class="form-control form-control-rounded m-b-sm" aria-describedby="roundedInputExample" placeholder="Email Address">
+                        <input type="password" class="form-control form-control-solid-bordered form-control-rounded" aria-describedby="roundedSolidBorderedInputExample" placeholder="Password">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php
+require_once('../includes/footer.php')
+?>
